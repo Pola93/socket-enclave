b36c5e5e25bd4493d7f4fb03bebc62a0407f641c
Mariusz Polka <mariusz.polka@idemia.com>
2022-12-13 08:20:03 +0100

    generate data key7

--- src/main/resources/enclave-kms/client.py
+++ src/main/resources/enclave-kms/client.py
@@ -8,6 +8,7 @@ import socket
 import sys
 import requests
 import rsa
+import base64
 
 
 class OrdinarySockListener:
@@ -114,7 +115,7 @@ def encrypt_message(message):
     print("message plain " + message)
     print("Private Key " + private_key_blob.hex())
     print("Public Key " + public_key_blob.hex())
-    print("Public Key base64 " + public_key_blob.decode())
+    print("Public Key base64 " + base64.b64encode(public_key_blob))
 
     public_key = rsa.PublicKey.load_pkcs1(public_key_blob)
     encrypted_message = rsa.encrypt(message.encode(), public_key)
