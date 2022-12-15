2bdac3fbc2cef0a377b32f1f43b89565161bdfa5
Mariusz Polka <mariusz.polka@idemia.com>
2022-12-15 16:43:02 +0100

    generate data key24

--- src/main/resources/enclave-kms/run.sh
+++ src/main/resources/enclave-kms/run.sh
@@ -8,11 +8,11 @@ ip addr add 127.0.0.1/32 dev lo
 ip link set dev lo up
 
 # Add a hosts record, pointing target site calls to local loopback
-#echo "127.0.0.1   ip-ranges.amazonaws.com" >> /etc/hosts
+echo "127.0.0.1   kms.eu-central-1.amazonaws.com" >> /etc/hosts
 
 touch /opt/app/libnsm.so
 
 # Run traffic forwarder in background and start the server
-#python3 /app/traffic_forwarder.py 127.0.0.1 443 3 8001 &
 cd /opt/app
+python3 traffic_forwarder.py 127.0.0.1 443 3 8000 &
 python3 server.py server 5005 6666
